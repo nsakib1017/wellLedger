d53eaa967ce37e567c70054d36e2927950014f62
NAZMUS SAKIB <sakib@priyo.com>
2022-07-08 21:12:53 +0600

    permission controller added

--- src/main/java/com/subsel/healthledger/core/controller/PermissionController.java
+++ src/main/java/com/subsel/healthledger/core/controller/PermissionController.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.subsel.healthledger.common.model.BaseController;
 import com.subsel.healthledger.core.model.ExtendTimeLimitPOJO;
-import okhttp3.Request;
 import org.hyperledger.fabric.gateway.*;
 
 import org.springframework.http.HttpHeaders;
@@ -21,15 +20,44 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping(path = "/api/users")
-public class UserController extends BaseController {
+@RequestMapping("/api/permissions")
+public class PermissionController extends BaseController {
 
-    @GetMapping(value="/{id}")
-    public ResponseEntity<Map<String, Object>> getUserData(@PathVariable String id, Request req) throws IOException, ContractException {
+    @PostMapping(value = "/revoke/{id}", produces = "application/json", consumes = "application/json")
+    public ResponseEntity<Map<String, Object>> revokePermission(@PathVariable String id) throws IOException, ContractException {
+        Map<String, Object> response = new HashMap<>();
+        HttpHeaders httpHeaders = new HttpHeaders();
 
         // Load a file system based wallet for managing identities.
         Path walletPath = Paths.get("wallet");
+        Wallet wallet = Wallets.newFileSystemWallet(walletPath);
+        // load a CCP
+        Path networkConfigPath = Paths.get("/Users/nsakibpriyo/go/src/github.com/nsakib1017/fabric-samples/fabcar/java/healthledger-2/src/main/java/com/subsel/healthledger/fabricNetwork/test-network/organizations/peerOrganizations/org1.example.com/connection-org1.yaml");
+
+        Gateway.Builder builder = Gateway.createBuilder();
+        builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
+
+        // create a gateway connection
+        try (Gateway gateway = builder.connect()) {
+
+            // get the network and contract
+            Network network = gateway.getNetwork("mychannel");
+            Contract contract = network.getContract("fabcar");
+
+            contract.evaluateTransaction("ChangeData", id, "no");
+            response.put("message", "Permission rvoked!!");
+
+            return new ResponseEntity<Map<String, Object>>(response, httpHeaders, HttpStatus.OK);
+        }
+    }
+
+    @PostMapping(value = "/extend/{id}", produces = "application/json", consumes = "application/json")
+    public ResponseEntity<Map<String, Object>> extendPermission(@PathVariable String id, @RequestBody ExtendTimeLimitPOJO extendTimeLimitPOJO) throws IOException, ContractException {
         Map<String, Object> response = new HashMap<>();
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        // Load a file system based wallet for managing identities.
+        Path walletPath = Paths.get("wallet");
         Wallet wallet = Wallets.newFileSystemWallet(walletPath);
         // load a CCP
         Path networkConfigPath = Paths.get("/Users/nsakibpriyo/go/src/github.com/nsakib1017/fabric-samples/fabcar/java/healthledger-2/src/main/java/com/subsel/healthledger/fabricNetwork/test-network/organizations/peerOrganizations/org1.example.com/connection-org1.yaml");
@@ -50,21 +78,20 @@ public class UserController extends BaseController {
             ObjectMapper mapper = new ObjectMapper();
             JsonNode actualObj = mapper.readTree(new String(result));
             String data = String.valueOf(actualObj.get("Data"));
+            int oldMaturity = Integer.parseInt(String.valueOf(actualObj.get("Maturity")));
 
             if (data.equals("yes")) {
-                if(Integer.parseInt(String.valueOf(actualObj.get("Maturity"))) < new Date().getTime()) {
+                if(oldMaturity < new Date().getTime()) {
                     contract.evaluateTransaction("ChangeData", id, "no");
                     response.put("message", "Token has expired");
                 } else {
-                    byte[] mainResult;
-                    mainResult = contract.evaluateTransaction("ReadEhr", String.valueOf(actualObj.get("Key")));
-                    JsonNode mainResultObject = mapper.readTree(new String(mainResult));
-                    response.put("result", mainResultObject);
+                    oldMaturity = oldMaturity + Integer.parseInt(extendTimeLimitPOJO.getLimit())*60*1000;
+                    contract.evaluateTransaction("ExtendLimit", id, String.valueOf(oldMaturity));
+                    response.put("message", "Limit extended");
                 }
             } else {
                 response.put("message", "Permission denied");
             }
-            HttpHeaders httpHeaders = new HttpHeaders();
             return new ResponseEntity<Map<String,Object>>(response, httpHeaders, HttpStatus.OK);
         }
     }
---
+++ src/main/java/com/subsel/healthledger/core/model/ExtendTimeLimitPOJO.java
@@ -0,0 +1,8 @@
+package com.subsel.healthledger.core.model;
+
+import lombok.Data;
+
+@Data
+public class ExtendTimeLimitPOJO {
+    String limit;
+}
